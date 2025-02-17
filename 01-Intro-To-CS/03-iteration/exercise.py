43cf3cd60d247193e92fbabccea6f22d341c57f5
KhaledSalhab <ksalhab89@hotmail.com>
2025-02-17 15:24:05 +0200

    added exercise.py for lecture 3

---
+++ 01-Intro-To-CS/03-iteration/exercise.py
@@ -0,0 +1,8 @@
+# Assume you are given a positive integer variable named N.
+# Write a piece of Python code that prints hello world on separate lines,
+# N times. You can use either a while loop or a for loop.
+
+n = int(input("pick a positive number: "))
+
+for i in range(n):
+    print("Hello world.")
